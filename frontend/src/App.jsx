221ddd94ee3f483970963306b224593aece707dc
Ganesh Chincholkar <gnesh.pc@gmail.com>
2024-03-12 23:17:43 +0530

    New CreateProject component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import Header from './components/header';
+import CreateProject from './components/CreateProject';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div>
       <Header></Header>
+      <CreateProject />
     </div>
   );
 };

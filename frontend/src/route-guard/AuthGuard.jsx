fb1d0601bd9e4101fa786eebb242cb72696f87e6
Ganesh Chincholkar <ganesh.chincholkar@siemens.com>
2024-03-17 14:38:06 +0530

    Auth guard component added

---
+++ frontend/src/route-guard/AuthGuard.jsx
@@ -0,0 +1,25 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const AuthGuard = ({ children }) => {
+  const { isLoggedIn } = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate('/login', { state: { from: location.pathname } });
+    }
+  }, [isLoggedIn, navigate, location.pathname]);
+
+  return children;
+};
+
+AuthGuard.propTypes = {
+  children: PropTypes.node,
+};
+
+export default AuthGuard;

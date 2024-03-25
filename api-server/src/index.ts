febf948df4134121a02d869a158c361dd06da05b
Ganesh Chincholkar <gnesh.pc@gmail.com>
2024-03-26 02:17:36 +0530

    Kafka started to recieve logs Logs saved to clickhouse databse

--- api-server/src/index.ts
+++ api-server/src/index.ts
@@ -3,14 +3,14 @@ dotenv.config();
 
 import logger from './logger/winston.config';
 import app from './app';
-// import { initKafka } from './kafka';
+import { initKafka } from './kafka';
 
 const PORT = process.env.PORT ?? 9000;
 
 const main = async () => {
   try {
     logger.info('starting api-server...');
-    // await initKafka();
+    await initKafka();
 
     app.listen(PORT, () => {
       logger.info(`api server listening on port: ${PORT}`);
--- api-server/src/kafka/index.ts
+++ api-server/src/kafka/index.ts
@@ -2,7 +2,11 @@ import fs from 'fs';
 import path from 'path';
 
 import logger from '../logger/winston.config';
+
 import { Kafka } from 'kafkajs';
+import { v4 as uuid } from 'uuid';
+
+import clickHouseClient from '../clickhouse';
 
 const broker = process.env.KAFKA_BROKER as string;
 const username = process.env.KAFKA_USERNAME as string;
@@ -22,11 +26,42 @@ const kafka = new Kafka({
   },
 });
 
-const consumer = kafka.consumer({ groupId: 'api-server-logs-consumer' });
+export const logConsumer = kafka.consumer({
+  groupId: 'api-server-logs-consumer',
+});
 
 export const initKafka = async () => {
-  await consumer.connect();
-  await consumer.subscribe({ topics: ['container-logs'] });
+  await logConsumer.connect();
+  await logConsumer.subscribe({ topics: ['container-logs'] });
+
+  await logConsumer.run({
+    eachBatch: async ({
+      batch,
+      heartbeat,
+      commitOffsetsIfNecessary,
+      resolveOffset,
+    }) => {
+      const messages = batch.messages.map((message) => {
+        return message.value?.toString();
+      });
+
+      logger.debug('received messages', messages);
+
+      for (const message of messages) {
+        if (message !== undefined) {
+          const { PROJECT_ID, DEPLOYMENT_ID, log } = JSON.parse(message);
+
+          clickHouseClient.insert({
+            table: 'log_events',
+            values: [{ event_id: uuid(), deployment_id: DEPLOYMENT_ID, log }],
+            format: 'JSONEachRow',
+          });
+        }
+      }
+
+      
+    },
+  });
 
   logger.info('kafka connection successful');
 };

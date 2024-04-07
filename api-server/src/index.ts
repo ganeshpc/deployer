6098d9b675104a9075d7d06425cbac04a2fbdddd
Ganesh Chincholkar <gnesh.pc@gmail.com>
2024-04-08 00:37:28 +0530

    redis added to api-server for container logs and status

--- api-server/src/index.ts
+++ api-server/src/index.ts
@@ -3,14 +3,15 @@ dotenv.config();
 
 import logger from './logger/winston.config';
 import app from './app';
-import { initKafka } from './kafka';
+import { initRedis } from './redis';
 
 const PORT = process.env.PORT ?? 9000;
 
 const main = async () => {
   try {
     logger.info('starting api-server...');
-    await initKafka();
+
+    await initRedis();
 
     app.listen(PORT, () => {
       logger.info(`api server listening on port: ${PORT}`);
---
+++ api-server/src/redis/index.ts
@@ -0,0 +1,67 @@
+import { Redis } from 'ioredis';
+
+import logger from '../logger/winston.config';
+import { saveLogs } from '../services/deployment';
+import * as projectService from '../services/project';
+
+const host = process.env.REDIS_HOST as string;
+const port = parseInt(process.env.REDIS_PORT as string);
+const username = process.env.REDIS_USERNAME as string;
+const password = process.env.REDIS_PASSWORD as string;
+
+const logChannleName = process.env.REDIS_LOG_CHANNEL as string;
+const deploymentStatusChannelName = process.env
+  .REDIS_DEPLOYMENT_STATUS_CHANNEL as string;
+
+export const initRedis = () => {
+  return new Promise<void>((resolve, reject) => {
+    const redis = new Redis({
+      host,
+      port,
+      username,
+      password,
+    });
+
+    redis.subscribe(
+      logChannleName,
+      deploymentStatusChannelName,
+      (err, count) => {
+        if (err) {
+          logger.error('Failed to subscribe to deployment-status: ', err);
+          reject(err);
+        } else {
+          logger.info(`Subscribed to ${count} channels`);
+          resolve();
+        }
+      }
+    );
+
+    redis.on('message', (channel, message) => {
+      const parsedMessage = JSON.parse(message);
+
+      switch (channel) {
+        case logChannleName:
+          logger.debug(`Received log message: ${message}`);
+
+          saveLogs(
+            parsedMessage.PROJECT_ID,
+            parsedMessage.DEPLOYMENT_ID,
+            parsedMessage.log
+          );
+
+          break;
+        case deploymentStatusChannelName:
+          logger.debug(`Received deployment status message: ${message}`);
+
+          projectService.setDeploymentStatus(
+            parsedMessage.DEPLOYMENT_ID,
+            parsedMessage.status
+          );
+
+          break;
+        default:
+          logger.error(`Received message from unknown channel: ${channel}`);
+      }
+    });
+  });
+};
